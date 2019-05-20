438bc55188476df4cd1f27537cfa13384897ca66
sakebook <sakebook@gmail.com>
2019-05-21 00:02:36 +0900

    Add CustomBrowser

--- src/main/kotlin/com/github/sakebook/slackal/Authorization.kt
+++ src/main/kotlin/com/github/sakebook/slackal/Authorization.kt
@@ -7,10 +7,14 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.Preconditions
 import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.calendar.CalendarScopes
+import java.awt.Desktop
 import java.io.*
+import java.net.URI
 import java.util.*
+import java.util.logging.Level
 
 class Authorization {
 
@@ -31,9 +35,9 @@ class Authorization {
             .setAccessType("offline")
             .build()
         val receiver = LocalServerReceiver.Builder()
-            .setPort(5000)
+            .setPort(3000)
             .build()
-        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+        return AuthorizationCodeInstalledApp(flow, receiver, CustomBrowser()).authorize("user")
     }
 
     private fun getReader(): Reader {
@@ -43,6 +47,44 @@ class Authorization {
             InputStreamReader(inputStream)
         } else {
             StringReader(clientSecret)
+        }
+    }
+}
+
+class CustomBrowser : AuthorizationCodeInstalledApp.Browser {
+
+    @Throws(IOException::class)
+    override fun browse(url: String) {
+        customBrowse(url)
+//        AuthorizationCodeInstalledApp.browse(url)
+    }
+
+    fun customBrowse(url: String) {
+        Preconditions.checkNotNull(url)
+        // Ask user to open in their browser using copy-paste
+        println("Please open the following address in your browser:")
+        println("  $url")
+        // Attempt to open it in the browser
+        try {
+            if (Desktop.isDesktopSupported()) {
+                val desktop = Desktop.getDesktop()
+                if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                    println("Attempting to open that address in the default browser now...")
+                    desktop.browse(URI.create(url))
+                } else {
+                    println("desktop.isSupported(Desktop.Action.BROWSE) is false")
+                }
+            }
+        } catch (e: IOException) {
+            println("Unable to open browser, $e")
+//            LOGGER.log(Level.WARNING, "Unable to open browser", e)
+        } catch (e: InternalError) {
+            // A bug in a JRE can cause Desktop.isDesktopSupported() to throw an
+            // InternalError rather than returning false. The error reads,
+            // "Can't connect to X11 window server using ':0.0' as the value of the
+            // DISPLAY variable." The exact error message may vary slightly.
+            println("Unable to open browser, $e")
+//            LOGGER.log(Level.WARNING, "Unable to open browser", e)
         }
     }
 }
